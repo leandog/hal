efe4253f4329f0d463c68c097b998f2bee454935
Jason Felice <jason.m.felice@gmail.com>
2011-12-28 23:03:56 -0500

    Handle XMPP style message

--- messagefilter.py
+++ messagefilter.py
@@ -3,5 +3,5 @@ import re
 
 class MessageFilter:
     def is_addressed_to_me(self, msg):
-        return re.search(r'\bhal\b', msg, re.IGNORECASE) != None
+        return re.search(r'\bhal\b', msg['body'], re.IGNORECASE) != None
 
--- test_messagefiler.py
+++ test_messagefiler.py
@@ -3,18 +3,18 @@ from messagefilter import *
 
 class Test_MessageFilter:
 
+    def message_with_body(self, body):
+        return {'body': body}
+
     def test_is_addressed_to_me_returns_False_for_arbitrary_message(self):
-        assert not MessageFilter().is_addressed_to_me('@joe, dude, what is up?')
+        assert not MessageFilter().is_addressed_to_me(self.message_with_body('@joe, dude, what is up?'))
 
     def test_is_addressed_to_me_returns_True_if_message_contains_at_hal(self):
-        assert MessageFilter().is_addressed_to_me('@hal, dude, what is up?')
+        assert MessageFilter().is_addressed_to_me(self.message_with_body('@hal, dude, what is up?'))
 
     def test_is_addressed_to_me_returns_False_if_message_contains_at_halbert(self):
-        assert not MessageFilter().is_addressed_to_me('@halbert, dude, what is up?')
+        assert not MessageFilter().is_addressed_to_me(self.message_with_body('@halbert, dude, what is up?'))
 
     def test_is_addressed_to_me_returns_True_for_Hal(self):
-        assert MessageFilter().is_addressed_to_me('Hal, open the pod bay doors.')
-
-
-
+        assert MessageFilter().is_addressed_to_me(self.message_with_body('Hal, open the pod bay doors.'))
 
