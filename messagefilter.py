31bd7a401cbfcbe82f0af70842a92db915ccb346
Jason Felice <jason.m.felice@gmail.com>
2011-12-28 23:17:29 -0500

    Not a replayed message by default

--- messagefilter.py
+++ messagefilter.py
@@ -5,3 +5,6 @@ class MessageFilter:
     def is_addressed_to_me(self, msg):
         return re.search(r'\bhal\b', msg['body'], re.IGNORECASE) != None
 
+    def is_replayed_message(self, msg):
+        return False
+
--- test_messagefiler.py
+++ test_messagefiler.py
@@ -4,7 +4,7 @@ from messagefilter import *
 class Test_MessageFilter:
 
     def message_with_body(self, body):
-        return {'body': body}
+        return {'id':'foo42', 'body': body}
 
     def test_is_addressed_to_me_returns_False_for_arbitrary_message(self):
         assert not MessageFilter().is_addressed_to_me(self.message_with_body('@joe, dude, what is up?'))
@@ -17,4 +17,7 @@ class Test_MessageFilter:
 
     def test_is_addressed_to_me_returns_True_for_Hal(self):
         assert MessageFilter().is_addressed_to_me(self.message_with_body('Hal, open the pod bay doors.'))
+
+    def test_is_replayed_message_returns_False_for_arbitrary_message(self):
+        assert not MessageFilter().is_replayed_message(self.message_with_body('Hello, world!'))
 
