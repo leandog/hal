ef95eb24bc0c6850049ff3683d3afa3fb98d764e
Jason Felice <jason.m.felice@gmail.com>
2011-12-28 23:31:03 -0500

    Add reply parameter to MessageHandler

--- messagehandler.py
+++ messagehandler.py
@@ -2,6 +2,10 @@
 import re
 
 class MessageHandler:
+
+    def __init__(self, reply_sender):
+        self.reply_sender = reply_sender
+
     def is_addressed_to_me(self, msg):
         return re.search(r'\bhal\b', msg['body'], re.IGNORECASE) != None
 
--- test_messagehandler.py
+++ test_messagehandler.py
@@ -7,23 +7,23 @@ class Test_MessageHandler:
         return {'id':'foo42', 'body': body}
 
     def test_is_addressed_to_me_returns_False_for_arbitrary_message(self):
-        assert not MessageFilter().is_addressed_to_me(self.message_with_body('@joe, dude, what is up?'))
+        assert not MessageHandler(None).is_addressed_to_me(self.message_with_body('@joe, dude, what is up?'))
 
     def test_is_addressed_to_me_returns_True_if_message_contains_at_hal(self):
-        assert MessageFilter().is_addressed_to_me(self.message_with_body('@hal, dude, what is up?'))
+        assert MessageHandler(None).is_addressed_to_me(self.message_with_body('@hal, dude, what is up?'))
 
     def test_is_addressed_to_me_returns_False_if_message_contains_at_halbert(self):
-        assert not MessageFilter().is_addressed_to_me(self.message_with_body('@halbert, dude, what is up?'))
+        assert not MessageHandler(None).is_addressed_to_me(self.message_with_body('@halbert, dude, what is up?'))
 
     def test_is_addressed_to_me_returns_True_for_Hal(self):
-        assert MessageFilter().is_addressed_to_me(self.message_with_body('Hal, open the pod bay doors.'))
+        assert MessageHandler(None).is_addressed_to_me(self.message_with_body('Hal, open the pod bay doors.'))
 
     def test_is_replayed_message_returns_False_for_arbitrary_message(self):
-        assert not MessageFilter().is_replayed_message(self.message_with_body('Hello, world!'))
+        assert not MessageHandler(None).is_replayed_message(self.message_with_body('Hello, world!'))
 
     def test_is_replayed_message_returns_True_when_id_is_empty_string(self):
-        assert MessageFilter().is_replayed_message({'body':'testing...', 'id':''})
+        assert MessageHandler(None).is_replayed_message({'body':'testing...', 'id':''})
 
     def test_is_replayed_message_returns_True_when_id_is_None(self):
-        assert MessageFilter().is_replayed_message({'body':'testing...', 'id':None})
+        assert MessageHandler(None).is_replayed_message({'body':'testing...', 'id':None})
 
